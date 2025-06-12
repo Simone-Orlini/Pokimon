45cf23edd920e58e51d177e7431463c9ea6a9373
Simone Orlini <simone.orl04@gmail.com>
2025-06-13 00:09:38 +0200

    Finally managed to display the tilemap correctly using the index of the tile. (Yippie c:).

--- Map/Tile.cs
+++ Map/Tile.cs
@@ -9,7 +9,7 @@ namespace Pokimon
 
         public int Width { get { return tileset.TileWidth; } }
         public int Height { get { return tileset.TileHeight; } }
-        public Vector2i TexturePosition { get { return CalculateTextureCoords();  } }
+        public int TexturePosition { get { return CalculateTextureCoords();  } }
         public int GridPosition { get { return CalculateGridPosition(); } }
         public int ID {  get { return id; } }
         public Tileset Tileset { get { return tileset; } }
@@ -25,12 +25,12 @@ namespace Pokimon
             return id;
         }
 
-        public Vector2i CalculateTextureCoords()
+        public int CalculateTextureCoords()
         {
             int x = id % tileset.Cols;
             int y = id / tileset.Cols;
-
-            return new Vector2i(x * Width, y * Height);
+            
+            return y * Height * tileset.TilesetTexture.Width + x * Width;
         }
     }
 }
