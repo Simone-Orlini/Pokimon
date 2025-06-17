62e6a94ef405fe952bac570cc25ca5e80d1da837
Simone Orlini <simone.orl04@gmail.com>
2025-06-17 12:28:48 +0200

    I forgor to add the files, now they are added c:

---
+++ Map/Pathfinding/PathfindingChunk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Pokimon
+{
+    public class PathfindingChunk : Chunk
+    {
+        public PathfindingChunk(XmlNode xmlChunk) : base(xmlChunk)
+        {
+
+        }
+    }
+}
---
+++ Map/Pathfinding/PathfindingLayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml;
+
+namespace Pokimon
+{
+    public class PathfindingLayer : Layer
+    {
+        public PathfindingLayer(XmlNode xmlLayer) : base(xmlLayer)
+        {
+            for(int i = 0; i < chunks.Length; i++)
+            {
+                chunks[i] = new PathfindingChunk(xmlChunks[i]);
+            }
+        }
+    }
+}
